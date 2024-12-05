d7a64014d44fb9a698580c35800b9b949477d440
Homebrew Tap <ci@kepler16.com>
2024-12-05 15:43:18 +0000

    Update kl to v2.10.2

--- Formula/kl.rb
+++ Formula/kl.rb
@@ -2,27 +2,27 @@ class Kl < Formula
   desc "Container orchestration and networking for micro-service development"
   homepage "https://github.com/kepler16/kl"
   license "MIT"
-  version "2.10.1"
+  version "2.10.2"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kepler16/kl/releases/download/v2.10.1/kl-macos-amd64.tar.gz"
-      sha256 "6f903c49e0b2bab7988e1603fd7d1897a5a7beac5856854be07ad1f01ec18986"
+      url "https://github.com/kepler16/kl/releases/download/v2.10.2/kl-macos-amd64.tar.gz"
+      sha256 "43662d5429c2e7a8c7a358485e1afec18837bd010d2dfc79034e9328d1e11f71"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kepler16/kl/releases/download/v2.10.1/kl-macos-arm64.tar.gz"
-      sha256 "f4978224d1af2f91660e7cda2b8a2483651c4e24fd72caa667c802298ed7223a"
+      url "https://github.com/kepler16/kl/releases/download/v2.10.2/kl-macos-arm64.tar.gz"
+      sha256 "296dcc87bba654d86a74819feb5807e7f08ce0921e175581ac690ea9ec2b4fdf"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kepler16/kl/releases/download/v2.10.1/kl-linux-amd64.tar.gz"
-      sha256 "9ead37f2f1ae9410b1cf45dda7515cfec454d7878317a8e94f8669e2fe8bde06"
+      url "https://github.com/kepler16/kl/releases/download/v2.10.2/kl-linux-amd64.tar.gz"
+      sha256 "fedda1bf169d7331ad15a3406d729e1d7db959e848702ee618433c4956777c63"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kepler16/kl/releases/download/v2.10.1/kl-linux-arm64.tar.gz"
-      sha256 "f688221abd6be2bad4017ab09206c0ca560f71c9df64e05e46d7c0dbcc0c7887"
+      url "https://github.com/kepler16/kl/releases/download/v2.10.2/kl-linux-arm64.tar.gz"
+      sha256 "bb7242924b8ed7aaf48babb0e996531e15a61da8d7c8cb57f4acdd127680308c"
     end
   end
 
